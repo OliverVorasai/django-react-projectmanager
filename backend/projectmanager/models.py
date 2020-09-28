4ce43dac19212b3f02127bdc8feff0234712797a
olivervorasai <olivervorasai@gmail.com>
2020-09-27 22:07:35 -0700

    initial commit

---
+++ backend/projectmanager/models.py
@@ -0,0 +1,13 @@
+from django.db import models
+
+# Create your models here.
+class Project(models.Model):
+    title = models.CharField(max_length=120)
+    description = models.TextField()
+    start = models.DateField()
+    deadline = models.DateField()
+    owner = models.EmailField()
+    completed = models.BooleanField(default=False)
+
+    def _str_(self):
+        return self.title
